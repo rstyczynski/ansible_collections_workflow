43273413b5a307b877e2c881768c04bc36b6cbfb
rstyczynski <rstyczynski@gmail.com>
2025-06-05 16:10:45 +0200

    .

---
+++ .github/workflows/oci_config2secrets.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SECTION="DEFAULT"
+
+function get_config() {
+    local SECTION=$1
+    awk -v section="[$SECTION]" '$0 == section { f=1; print; next } /^\[/ { f=0 } f ' ~/.oci/config
+}
+
+echo 
+echo OCI_CONFIG_BASE64
+echo ==================
+get_config $SECTION | grep -v key_file= | base64
+
+echo
+echo OCI_API_KEY_BASE64
+echo ==================
+key_file=$(get_config $SECTION | grep key_file= | cut -d= -f2)
+base64 -i $key_file
+
